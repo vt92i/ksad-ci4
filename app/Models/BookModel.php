2669289be02045295c141ed95b76fa4be50d3871
ethan <benidiktusvio@gmail.com>
2022-05-04 14:52:39 +0700

    Update BookModel.php

--- app/Models/BookModel.php
+++ app/Models/BookModel.php
@@ -8,8 +8,18 @@ class BookModel extends Model {
     protected $table = 'book';
     protected $primaryKey = 'book_id';
     protected $useTimestamps = true;
-    protected $allowedFields = ['title', 'slug', 'author', 'release_year', 'price', 'discount', 'stock', 'book_category_id', 'cover'];
     protected $useSoftDeletes = true;
+    protected $allowedFields = [
+        'title',
+        'slug',
+        'author',
+        'release_year',
+        'price',
+        'discount',
+        'stock',
+        'book_category_id',
+        'cover',
+    ];
 
     public function getAllBooks() {
         $this->table('book')->join('book_category', 'book.book_category_id = book_category.book_category_id')->where(['deleted_at' => null]);
